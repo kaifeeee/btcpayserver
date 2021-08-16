c29e38efd77fe64e00a1a60d0f21d43deb361049
Kukks <evilkukka@gmail.com>
2021-08-16 10:07:49 +0200

    LNURL Payment Method Support

--- BTCPayServer/Controllers/InvoiceController.UI.cs
+++ BTCPayServer/Controllers/InvoiceController.UI.cs
@@ -378,7 +378,8 @@ namespace BTCPayServer.Controllers
                             Overpaid = _CurrencyNameTable.DisplayFormatCurrency(
                                 accounting.OverpaidHelper.ToDecimal(MoneyUnit.BTC), paymentMethodId.CryptoCode),
                             Address = data.GetPaymentMethodDetails().GetPaymentDestination(),
-                            Rate = ExchangeRate(data)
+                            Rate = ExchangeRate(data),
+                            PaymentMethodRaw = data
                         };
                     }).ToList()
             };
